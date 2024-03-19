dff5d3e7f1a5bb7e8d54c617df146edce331c6ef
jprice <jprice@izeni.com>
2024-03-18 23:05:34 -0600

    Configure media and static files

--- backend/progress_tracker/settings.py
+++ backend/progress_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "accounts",
 ]
 
@@ -121,9 +122,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = str(BASE_DIR / "dev" / "static")
 STATIC_URL = "static/"
+
+MEDIA_ROOT = str(BASE_DIR / "dev" / "media")
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+####################################
+#        3RD PARTY SETTINGS        #
+####################################
+
+
+# django-cors-headers
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+
+# djangorestframework
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}

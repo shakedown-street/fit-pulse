295c1ea9d74ce5df2f069c5851df18a426be812a
jprice <jprice@izeni.com>
2024-03-19 14:09:34 -0600

    Update ExerciseForm to work for updating as well as creating

--- frontend/src/exercises/ExerciseForm.tsx
+++ frontend/src/exercises/ExerciseForm.tsx
@@ -1,9 +1,11 @@
 import { useForm } from 'react-hook-form';
 import { Button, Input } from '~/ui';
+import { Exercise } from './Exercise';
 import './ExerciseForm.scss';
 
 export type ExerciseFormProps = {
-  onSubmit: (data: ExerciseFormData) => void;
+  instance?: Exercise;
+  onSubmit: (data: ExerciseFormData, instance?: Exercise) => void;
 };
 
 export type ExerciseFormData = {
@@ -11,11 +13,16 @@ export type ExerciseFormData = {
   value_type: 'weight' | 'reps' | 'time' | 'bpm';
 };
 
-export const ExerciseForm = ({ onSubmit }: ExerciseFormProps) => {
-  const exerciseForm = useForm<ExerciseFormData>();
+export const ExerciseForm = ({ instance, onSubmit }: ExerciseFormProps) => {
+  const exerciseForm = useForm<ExerciseFormData>({
+    defaultValues: {
+      name: instance ? instance.name : '',
+      value_type: instance ? instance.value_type : 'weight',
+    },
+  });
 
   return (
-    <form className="ExerciseForm" onSubmit={exerciseForm.handleSubmit(onSubmit)}>
+    <form className="ExerciseForm" onSubmit={exerciseForm.handleSubmit((data) => onSubmit(data, instance))}>
       <Input fluid id="name" label="Name" {...exerciseForm.register('name', { required: true })} />
       <div className="Input__container Input__container--fluid">
         <label className="Input__label" htmlFor="value_type">
@@ -23,6 +30,7 @@ export const ExerciseForm = ({ onSubmit }: ExerciseFormProps) => {
         </label>
         <select
           className="Input Input--fluid"
+          disabled={!!instance}
           id="value_type"
           {...exerciseForm.register('value_type', { required: true })}
         >
@@ -32,8 +40,9 @@ export const ExerciseForm = ({ onSubmit }: ExerciseFormProps) => {
           <option value="bpm">BPM</option>
         </select>
       </div>
+      {instance && <p className="hint">Exercise type cannot be changed once it has been created.</p>}
       <Button color="primary" disabled={!exerciseForm.formState.isValid} fluid type="submit" variant="raised">
-        Create
+        {instance ? 'Update' : 'Create'}
       </Button>
     </form>
   );

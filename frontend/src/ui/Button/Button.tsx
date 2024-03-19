1ab3cb061352f3e8922e60e60fff816b1d9a7fd6
jprice <jprice@izeni.com>
2024-03-19 12:20:26 -0600

    Finish Button component variants, size, colors and other props

--- frontend/src/ui/Button/Button.tsx
+++ frontend/src/ui/Button/Button.tsx
@@ -2,18 +2,47 @@ import clsx from 'clsx';
 import React from 'react';
 import './Button.scss';
 
+export type ButtonColor =
+  | 'primary'
+  | 'gray'
+  | 'red'
+  | 'pink'
+  | 'grape'
+  | 'violet'
+  | 'indigo'
+  | 'blue'
+  | 'cyan'
+  | 'teal'
+  | 'green'
+  | 'lime'
+  | 'yellow'
+  | 'orange';
+export type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type ButtonVariant = 'text' | 'outlined' | 'ghost' | 'raised';
+
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  color?: ButtonColor;
   fluid?: boolean;
+  rounded?: boolean;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
 };
 
 export const Button = React.forwardRef(
-  ({ className, fluid, type = 'button', ...rest }: ButtonProps, ref: React.ForwardedRef<HTMLButtonElement>) => {
+  (
+    { className, color = 'gray', fluid, rounded, size = 'md', type = 'button', variant = 'text', ...rest }: ButtonProps,
+    ref: React.ForwardedRef<HTMLButtonElement>,
+  ) => {
     return (
       <button
         className={clsx(
           'Button',
           {
             'Button--fluid': fluid,
+            'Button--rounded': rounded,
+            [`Button--color--${color}`]: color,
+            [`Button--size--${size}`]: size,
+            [`Button--variant--${variant}`]: variant,
           },
           className,
         )}

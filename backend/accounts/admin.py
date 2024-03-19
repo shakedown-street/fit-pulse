d0bc324672cca91782041b221d0a71edd800bea1
jprice <jprice@izeni.com>
2024-03-18 23:25:17 -0600

    Only return self in UserViewSet for now

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,8 +1,7 @@
+from accounts.models import User
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-
-from accounts.models import User
 
 
 @admin.register(User)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-
 from accounts.permissions import UserPermission
 from accounts.serializers import UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
 User = get_user_model()
 
@@ -11,3 +10,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (UserPermission,)
+
+    def get_queryset(self):
+        qs = self.queryset
+        qs = qs.filter(id=self.request.user.id)
+        return qs

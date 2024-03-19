5229bf599951bab34e9f2771f65a3ba085c45de6
jprice <jprice@izeni.com>
2024-03-19 15:10:28 -0600

    Add ordering for Exercise and Progress models

--- backend/exercises/models.py
+++ backend/exercises/models.py
@@ -18,8 +18,14 @@ class Exercise(BaseMixin):
     name = models.CharField(max_length=256)
     value_type = models.CharField(max_length=16, choices=VALUE_TYPE_CHOICES)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return self.name
+
+    def user_progress_count(self, user):
+        return Progress.objects.filter(exercise=self, user=user).count()
 
 
 class Progress(BaseMixin):
@@ -31,6 +37,10 @@ class Progress(BaseMixin):
     class Meta:
         verbose_name = "progress"
         verbose_name_plural = "progress"
+        ordering = (
+            "-date",
+            "exercise__name",
+        )
 
     def __str__(self):
         return f"{self.user.get_full_name()} - {self.exercise.name} - {self.date}"

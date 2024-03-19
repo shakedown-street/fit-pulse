06d717d8c0c907204d2be973704ebf936a302a72
jprice <jprice@izeni.com>
2024-03-19 14:12:12 -0600

    Wireup creating and deleting exercises on the Home page route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,24 +2,49 @@ import React from 'react';
 import { RouteObject } from 'react-router-dom';
 import { AppLayout } from './AppLayout';
 import { LoginRoute, useAuth } from './auth';
-import { ExerciseForm, ExerciseFormData, ProgressForm, ProgressFormData } from './exercises';
-import { http } from './http';
+import { Exercise, ExerciseForm, ExerciseFormData, ExerciseTable, ProgressForm, ProgressFormData } from './exercises';
+import { ListResponse, http } from './http';
 import { Button, Container, RadixDialog } from './ui';
 
 const Home = () => {
+  const [exercises, setExercises] = React.useState<Exercise[]>([]);
   const [exerciseDialogOpen, setExerciseDialogOpen] = React.useState(false);
+  const [exerciseDialogInstance, setExerciseDialogInstance] = React.useState<Exercise | undefined>();
   const [progressDialogOpen, setProgressDialogOpen] = React.useState(false);
+  const [deleteExerciseDialogOpen, setDeleteExerciseDialogOpen] = React.useState(false);
 
   const { user } = useAuth();
 
-  function submitExerciseForm(data: ExerciseFormData) {
-    http.post('/api/exercises/', data).then(() => {
-      setExerciseDialogOpen(false);
+  React.useEffect(() => {
+    http.get<ListResponse<Exercise>>('/api/exercises').then((exercises) => setExercises(exercises.data.results));
+  }, []);
+
+  function submitExerciseForm(data: ExerciseFormData, instance?: Exercise) {
+    if (instance) {
+      http.patch<Exercise>(`/api/exercises/${instance.id}/`, data).then((exercise) => {
+        setExercises((exercises) => exercises.map((e) => (e.id === exercise.data.id ? exercise.data : e)));
+        setExerciseDialogOpen(false);
+      });
+    } else {
+      http.post<Exercise>('/api/exercises/', data).then((exercise) => {
+        setExercises((exercises) => [...exercises, exercise.data]);
+        setExerciseDialogOpen(false);
+      });
+    }
+  }
+
+  function confirmDeleteExercise() {
+    if (!exerciseDialogInstance) {
+      return;
+    }
+    http.delete(`/api/exercises/${exerciseDialogInstance.id}`).then(() => {
+      setExercises((exercises) => exercises.filter((e) => e.id !== exerciseDialogInstance.id));
+      setDeleteExerciseDialogOpen(false);
     });
   }
 
   function submitProgressForm(data: ProgressFormData) {
-    http.post('/api/exercises/', data).then(() => {
+    http.post('/api/progress/', data).then(() => {
       setProgressDialogOpen(false);
     });
   }
@@ -28,9 +53,27 @@ const Home = () => {
     <>
       <Container>
         <h1>Home</h1>
+        <ExerciseTable
+          exercises={exercises}
+          onDelete={(exercise) => {
+            setExerciseDialogInstance(exercise);
+            setDeleteExerciseDialogOpen(true);
+          }}
+          onUpdate={(exercise) => {
+            setExerciseDialogInstance(exercise);
+            setExerciseDialogOpen(true);
+          }}
+        />
         {user && (
           <div className="flex gap-2">
-            <Button color="primary" onClick={() => setExerciseDialogOpen(true)} variant="ghost">
+            <Button
+              color="primary"
+              onClick={() => {
+                setExerciseDialogInstance(undefined);
+                setExerciseDialogOpen(true);
+              }}
+              variant="ghost"
+            >
               Create Exercise
             </Button>
             <Button color="primary" onClick={() => setProgressDialogOpen(true)} variant="ghost">
@@ -38,13 +81,59 @@ const Home = () => {
             </Button>
           </div>
         )}
-        <RadixDialog className="p-4" open={exerciseDialogOpen} onOpenChange={setExerciseDialogOpen}>
-          <h2 className="mb-2">Create Exercise</h2>
-          <ExerciseForm onSubmit={submitExerciseForm} />
+        <RadixDialog
+          className="p-4"
+          open={exerciseDialogOpen}
+          onOpenChange={(open) => {
+            if (!open) {
+              setExerciseDialogInstance(undefined);
+            }
+            setExerciseDialogOpen(open);
+          }}
+          style={{
+            width: '320px',
+          }}
+        >
+          <h2 className="mb-2">{exerciseDialogInstance ? 'Update' : 'Create'} Exercise</h2>
+          <ExerciseForm instance={exerciseDialogInstance} onSubmit={submitExerciseForm} />
         </RadixDialog>
-        <RadixDialog className="p-4" open={progressDialogOpen} onOpenChange={setProgressDialogOpen}>
+        <RadixDialog
+          className="p-4"
+          open={progressDialogOpen}
+          onOpenChange={setProgressDialogOpen}
+          style={{
+            width: '320px',
+          }}
+        >
           <h2 className="mb-2">Track Progress</h2>
           <ProgressForm onSubmit={submitProgressForm} />
+        </RadixDialog>
+        <RadixDialog
+          className="p-4"
+          open={deleteExerciseDialogOpen}
+          onOpenChange={(open) => {
+            if (!open) {
+              setExerciseDialogInstance(undefined);
+            }
+            setDeleteExerciseDialogOpen(open);
+          }}
+        >
+          <h2 className="mb-2">Delete Exercise</h2>
+          <p>Are you sure you want to delete this exercise?</p>
+          <div className="flex justify-end gap-2 mt-4">
+            <Button
+              onClick={() => {
+                setExerciseDialogInstance(undefined);
+                setDeleteExerciseDialogOpen(false);
+              }}
+              variant="ghost"
+            >
+              Cancel
+            </Button>
+            <Button color="red" onClick={confirmDeleteExercise} variant="ghost">
+              Delete
+            </Button>
+          </div>
         </RadixDialog>
       </Container>
     </>

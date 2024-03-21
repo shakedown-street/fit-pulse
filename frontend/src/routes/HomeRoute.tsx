d54d40f9ee26a8215da464fa1dc908929c729e81
jprice <jprice@izeni.com>
2024-03-21 13:42:28 -0600

    HomeRoute text changes

--- frontend/src/routes/HomeRoute.tsx
+++ frontend/src/routes/HomeRoute.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Container } from '~/ui';
 
 export const HomeRoute = () => {
@@ -8,11 +9,11 @@ export const HomeRoute = () => {
           <h1>Fit Pulse</h1>
           <h4 className="my-4">Welcome to FitPulse</h4>
           <p style={{ maxWidth: '768px' }}>
-            To get started, log in and click "Exercises" at the top of the page. From there you can add exercises you
-            want to begin tracking your improvement on. Once you have added some exercises, you can being logging your
-            performances on those exercises. You can view your performances on an exercise by clicking into the exercise
-            and viewing the performance log. Your performance log will show you how much you have improved on the
-            exercise over time.
+            To get started, <Link to="/login">login</Link> and navigate to the <Link to="/exercises">exercises</Link>{' '}
+            page. From there you can add exercises you want to begin tracking your improvement on. Once you have added
+            some exercises, you can begin logging your performances on those exercises. You can view your performances
+            on an exercise by clicking into the exercise and viewing the performance log. Your performance log will show
+            you how much you have improved on the exercise over time.
           </p>
         </div>
       </Container>

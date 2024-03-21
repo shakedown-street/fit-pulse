cf53d7cebfe55466939f04efd4acbb21cfe5e9d1
jprice <jprice@izeni.com>
2024-03-21 09:57:12 -0600

    Add RadixTooltip component

--- frontend/src/ui/index.ts
+++ frontend/src/ui/index.ts
@@ -4,3 +4,4 @@ export * from './Container/Container';
 export * from './IconButton/IconButton';
 export * from './Input/Input';
 export * from './RadixDialog/RadixDialog';
+export * from './RadixTooltip/RadixTooltip';

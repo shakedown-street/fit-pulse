a747333b419e7c40eb62106342a2992af47778ec
jprice <jprice@izeni.com>
2024-03-22 10:32:19 -0600

    Add Avatar component

---
+++ frontend/src/ui/Avatar/Avatar.tsx
@@ -0,0 +1,43 @@
+import { User } from '~/types';
+import './Avatar.scss';
+import React from 'react';
+import clsx from 'clsx';
+
+export type AvatarSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type AvatarProps = React.HTMLAttributes<HTMLDivElement> & {
+  size?: AvatarSize;
+  user: User;
+};
+
+export const Avatar = React.forwardRef(
+  ({ size = 'md', user, ...rest }: AvatarProps, ref: React.ForwardedRef<HTMLDivElement>) => {
+    function getName() {
+      if (user.full_name) {
+        return user.full_name;
+      } else {
+        return user.username;
+      }
+    }
+
+    function getInitials() {
+      return getName()
+        .split(' ')
+        .map((name) => name[0])
+        .join('');
+    }
+
+    return (
+      <div
+        className={clsx('Avatar', {
+          [`Avatar--size--${size}`]: size,
+        })}
+        ref={ref}
+        {...rest}
+      >
+        {user.image && <img className="Avatar__image" src={user.image} alt={getName()} />}
+        {!user.image && <div className="Avatar__fallback">{getInitials()}</div>}
+      </div>
+    );
+  },
+);

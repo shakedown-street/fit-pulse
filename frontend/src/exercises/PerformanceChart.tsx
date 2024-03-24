39936c4ebd44e5a47f818cf153744e82d3d00e3b
jprice <jprice@izeni.com>
2024-03-24 02:02:42 -0600

    Update PerformanceChart to show multiple metrics

--- frontend/src/exercises/PerformanceChart.tsx
+++ frontend/src/exercises/PerformanceChart.tsx
@@ -1,39 +1,29 @@
 import { format } from 'date-fns';
 import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
-import { Exercise, Performance } from '~/types';
+import { Performance } from '~/types';
 import { parseDateString } from '~/utils/parseDateString';
 import './PerformanceChart.scss';
 
 export type PerformanceChartProps = {
-  exercise: Exercise;
   performances: Performance[];
 };
 
-export const PerformanceChartTooltip = ({ active, payload, label, exercise }: any) => {
-  function getValueLabel() {
-    switch (exercise.value_type) {
-      case 'weight':
-        return 'Weight (lbs)';
-      case 'reps':
-        return 'Reps';
-      case 'time':
-        return 'Time (seconds)';
-      case 'bpm':
-        return 'BPM';
-    }
-  }
-
+export const PerformanceChartTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
     return (
       <div className="PerformanceChartTooltip">
         <p className="PerformanceChartTooltip__label">{label}</p>
-        <p className="PerformanceChartTooltip__value">{`${getValueLabel()}: ${payload[0].value}`}</p>
+        {payload.map((p: any) => (
+          <p key={p.dataKey} className="PerformanceChartTooltip__value">
+            {`${p.name}: ${p.value}`}
+          </p>
+        ))}
       </div>
     );
   }
 };
 
-export const PerformanceChart = ({ exercise, performances }: PerformanceChartProps) => {
+export const PerformanceChart = ({ performances }: PerformanceChartProps) => {
   const data = performances
     .slice()
     .sort((a, b) => {
@@ -44,11 +34,32 @@ export const PerformanceChart = ({ exercise, performances }: PerformanceChartPro
       return a.created_at.localeCompare(b.created_at);
     })
     .map((p) => {
+      const metrics = p.metrics.reduce(
+        (acc, pm) => {
+          acc[pm.metric.name] = pm.value;
+          return acc;
+        },
+        {} as { [key: string]: number },
+      );
       return {
         date: format(parseDateString(p.date, 'yyyy-MM-dd'), 'MM/dd/yy'),
-        value: p.value,
+        ...metrics,
       };
     });
+
+  const metricNames = Array.from(new Set(performances.flatMap((p) => p.metrics.map((pm) => pm.metric.name))));
+
+  const metricColors = [
+    '#5f3dc4', // violet 9
+    '#364fc7', // indigo 9
+    '#1862ab', // blue 9
+    '#0b7285', // cyan 9
+    '#087f5b', // teal 9
+    '#2b8a3e', // green 9
+    '#5c940d', // lime 9
+    '#e67700', // yellow 9
+    '#d9480f', // orange 9
+  ];
 
   return (
     <ResponsiveContainer width="100%" height={300}>
@@ -62,16 +73,27 @@ export const PerformanceChart = ({ exercise, performances }: PerformanceChartPro
         }}
       >
         <defs>
-          <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#5f3dc4" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#5f3dc4" stopOpacity={0.2} />
-          </linearGradient>
+          {metricNames.map((name, idx) => (
+            <linearGradient key={idx} id={`color${name}`} x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={metricColors[idx]} stopOpacity={0.8} />
+              <stop offset="95%" stopColor={metricColors[idx]} stopOpacity={0.2} />
+            </linearGradient>
+          ))}
         </defs>
-        <Area dataKey="value" stroke="#5f3dc4" fillOpacity={1} fill="url(#colorValue)" type="monotone" />
+        {metricNames.map((name, idx) => (
+          <Area
+            key={name}
+            dataKey={name}
+            stroke={metricColors[idx]}
+            fillOpacity={1}
+            fill={`url(#color${name})`}
+            type="monotone"
+          />
+        ))}
         <CartesianGrid stroke="#212529" strokeDasharray="3 3" />
         <XAxis dataKey="date" hide />
-        <YAxis width={48} />
-        <Tooltip content={<PerformanceChartTooltip exercise={exercise} />} />
+        <YAxis hide />
+        <Tooltip content={<PerformanceChartTooltip />} />
       </AreaChart>
     </ResponsiveContainer>
   );

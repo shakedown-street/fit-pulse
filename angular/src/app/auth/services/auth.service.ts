dbeee3a8ebba45f435610893eebefa0b7bc20947
jprice <jprice@izeni.com>
2024-07-18 13:34:41 -0600

    Add environments

--- angular/src/app/auth/services/auth.service.ts
+++ angular/src/app/auth/services/auth.service.ts
@@ -1,6 +1,11 @@
 import { User } from '@/app/types/User';
+import { environment } from '@/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
+
+function buildUrl(path: string) {
+  return `${environment.apiUrl}${path}`;
+}
 
 export type LoginRequest = {
   username: string;
@@ -21,20 +26,20 @@ export class AuthService {
   public user = signal<User | undefined>(undefined);
 
   constructor(private http: HttpClient) {
-    this.http.get<User>('http://localhost:8000/api/session/').subscribe((user) => {
+    this.http.get<User>(buildUrl('/api/session/')).subscribe((user) => {
       this.user.set(user);
     });
   }
 
   public login(payload: LoginRequest) {
-    return this.http.post<User>('http://localhost:8000/api/login/', payload);
+    return this.http.post<User>(buildUrl('/api/login/'), payload);
   }
 
   public logout() {
-    return this.http.post('http://localhost:8000/api/logout/', {});
+    return this.http.post(buildUrl('/api/logout/'), {});
   }
 
   public signup(payload: SignupRequest) {
-    return this.http.post<User>('http://localhost:8000/api/users/', payload);
+    return this.http.post<User>(buildUrl('/api/users/'), payload);
   }
 }

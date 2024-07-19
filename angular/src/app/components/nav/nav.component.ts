b9a6c1bbefb0e4724c52daf072822d6a07424b40
jprice <jprice@izeni.com>
2024-07-18 21:51:53 -0600

    Update nav to use new popover and user avatar components

--- angular/src/app/components/nav/nav.component.ts
+++ angular/src/app/components/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import { AuthService } from '@/app/auth';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { PopoverComponent } from '../popover/popover.component';
+import { UserAvatarComponent } from '@/app/auth/components/user-avatar/user-avatar.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [PopoverComponent, RouterModule, UserAvatarComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
 export class NavComponent {
+  public userMenuOpen = false;
+
   constructor(
     public auth: AuthService,
     private router: Router,
